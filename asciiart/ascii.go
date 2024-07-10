127988dec2b20190f761fb89cf20f3ddc65e0e3c
Cherrypick14 <cherrypicklet@gmail.com>
2024-07-10 16:11:08 +0300

    [Fix] : Remove typo

--- asciiart/ascii.go
+++ asciiart/ascii.go
@@ -9,20 +9,11 @@ func AsciiArt(words, contents []string) string {
 	var countSpace int
 	var artBuild strings.Builder // Using strings.Builder for efficient string concatenation.
 
-	sub := false // checker
-	position := 0  
-	subStr := "he"
 	for _, word := range words {
-		indexs := subIndexs(word, subStr)
-
 		if word != "" {
 			for i := 0; i < 8; i++ { // Each character is represented by 8 lines of ASCII art.
-				for j, char := range word {
-					if validIndex(j, indexs){
-						artBuild.WriteString("\033[32m")
-						sub = true
-						position = j
-					}
+				for _, char := range word {
+
 					if char == '\n' {
 						continue
 					}
@@ -33,9 +24,6 @@ func AsciiArt(words, contents []string) string {
 					// Append the corresponding ASCII art line for the character.
 					artBuild.WriteString(contents[int(char-' ')*9+1+i])
 
-					if sub && j == position+len(subStr)-1{
-						artBuild.WriteString("\033[37m")
-					}
 				}
 				artBuild.WriteRune('\n')
 			}
@@ -47,30 +35,4 @@ func AsciiArt(words, contents []string) string {
 		}
 	}
 	return artBuild.String() // Return the constructed ASCII art as a string.
-}
-
-
-func subIndexs(s, subStr string)[]int{
-    index := []int{}
-    leftCharacters := 0
-    
-    for{
-        idx:= strings.Index(s, subStr)
-        if idx == -1{
-            break
-        }
-        index = append(index, idx+leftCharacters)
-        s = s[idx+len(subStr):]
-        leftCharacters += idx+len(subStr)
-    }
-    return index
-}
-
-func validIndex(index int, indexs []int)bool{
-	for _, idx := range indexs{
-		if index == idx{
-			return true
-		}
-	}
-	return false
 }
